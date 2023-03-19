f03f424b00e78c99aa60129ee0fd7167e2fa5b98
Arnab Banik <bsse1230@iit.du.ac.bd>
2023-03-20 05:06:37 +0600

    added google login

---
+++ code/app/Http/Controllers/GoogleLoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+
+
+class GoogleLoginController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function login(){
+        try{
+            $googleUser = Socialite::driver('google')->user();
+            $user = User::where('google_id', $googleUser->getId())->first();
+            
+
+
+            if (!$user){
+
+                // dd ("user doesn't exist");
+
+                $newUser = User::create([
+                    'google_id' => $googleUser->getId(),
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                ]);
+
+                Auth::login($newUser);
+
+                return redirect()->intended('dashboard');
+            } 
+            else {
+                Auth::login($user, true);
+                return redirect()->intended('dashboard');
+            }
+        } catch(\Throwable $th){
+            dd('Something went wrong!'. $th->getMessage());
+        }
+    }
+}
