b129bf97e409d65a3f248a6414e511a068b30451
Arnab Banik <bsse1230@iit.du.ac.bd>
2023-05-24 23:06:42 +0600

    working on viewing submitted article by the journal admin (incomplete)

--- code/app/Http/Controllers/article/postArticleSubmissionController.php
+++ code/app/Http/Controllers/article/postArticleSubmissionController.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\article;
 
+use App\Http\Controllers\Controller;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
 class postArticleSubmissionController extends Controller
 {
-    public function viewArticle($id)
+    public function viewArticle(Article $article)
     {
-        $article = Article::find($id);
+        // $article = Article::find($id);
 
-        return view('')
+        return view('layouts.dashboard.editorAndJournalAdmin.viewArticle', compact('article'));
     }
 }
